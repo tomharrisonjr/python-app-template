f757c4674f4198da59eaaccc6ecf61288d44785a
Tom Harrison <tomharrisonjr@gmail.com>
2024-01-01 17:42:15 -0500

    feat: unit tests

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+import pytest
+from httpx import AsyncClient, Response
+from respx import MockRouter
+
+from python_app_template.app import app
+
+
+@pytest.mark.asyncio
+async def test_get_pokemon(respx_mock: MockRouter) -> None:
+    expected_response: dict = {"pokemon": " charmandera"}
+    number: int = 4
+
+    respx_mock.get(
+        url=f"https://pokeapi.co/api/v2/pokemon/{number}"
+    ).return_value = Response(status_code=200, json=expected_response)
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        url: str = f"/{number}"
+        response: Response = await client.get(url=url)
+
+        assert response.json() == expected_response
